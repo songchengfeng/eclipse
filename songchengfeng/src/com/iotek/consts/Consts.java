1f38c3f1f3bd6fa48b1f4fcc4bb7c4e915ce48ec
songchengfeng <41041562+songchengfeng@users.noreply.github.com>
2023-10-01 21:32:12 +0800

    Delete songchengfeng directory

--- songchengfeng/src/com/iotek/consts/Consts.java
+++
@@ -1,5 +0,0 @@
-package com.iotek.consts;
-
-public class Consts {
-	public final static int PAGE_SIZE = 5;
-}
--- songchengfeng/src/com/iotek/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.iotek.service;
-
-import com.iotek.entity.User;
-
-public interface UserService {
-
-	public User queryUser(String username, String psw);
-}
